cc3e503dcc0e150d878f6f55b3065699db658e5d
Felipe Paim Roos <felipepaimroos@gmail.com>
2024-05-20 17:39:12 -0300

    Configurando a primeira com condição

---
+++ app/src/main/java/br/com/alura/aluraesporte/ui/viewmodel/LoginViewModel.kt
@@ -0,0 +1,15 @@
+package br.com.alura.aluraesporte.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import br.com.alura.aluraesporte.repository.LoginRepository
+import br.com.alura.aluraesporte.repository.ProdutoRepository
+
+class LoginViewModel(private val repository: LoginRepository) : ViewModel(){
+
+    fun loga(){
+        repository.loga()
+    }
+
+    fun estaLogado(): Boolean = repository.estaLogado()
+
+}
